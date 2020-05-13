424c8005323e3c9f40285ea200b7d766cda1f3f9
RyutoKawasaki <various.use.0416@gmail.com>
2020-05-13 22:27:51 +0900

    add logo

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
+    <a
+      href="https://www.notion.so/Ryuto-Kawasaki-Portfolio-a6f9d9a28a2c47e4a81cc61077e2a421"
+      target="_blank"
+      rel="noopener noreferrer"
+      ><img class="logo" src="~/assets/ryt_kwzacky_logo.png"
+    /></a>
     <nuxt />
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.logo {
+  height: 80px;
+  width: 80px;
+  position: absolute;
+  top: 30px;
+  left: 30px;
+}
+</style>
